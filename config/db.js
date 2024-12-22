0764be85dae1512d29b0775fc366f08c0eeee226
chandini <chandinisureshkumar@gmail.com>
2024-12-22 10:42:27 +0530

    initial

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
 // import the module mongoose
 const mongoose = require("mongoose");
-const MONGO_URI = "mongodb://127.0.0.1:27017/Recipe";
-// const MONGO_URI = "mongodb://localhost:27017/Recipe";
 
 // connect to mongodb 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB is connected")
     } catch (error) {
         console.log("Error in conecting the MongoDB", error);
